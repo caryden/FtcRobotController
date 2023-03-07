84a5827314498a370b67382598f2cd00b9b1909f
Carl Ryden <caryden@alum.mit.edu>
2023-03-07 17:50:28 -0500

    Added mockito with a working sample for TestDriveTests

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/tests/commands/TestDriveTests.kt
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/tests/commands/TestDriveTests.kt
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode.tests.commands
 
+//import io.mockk.mockk
+import org.firstinspires.ftc.teamcode.commands.TestDrive
+import org.firstinspires.ftc.teamcode.subsystems.LeapfrogDriveBase
 import org.junit.Assert.*
 
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.verify
+import java.util.function.DoubleSupplier
 
 class TestDriveTests {
 
@@ -19,15 +26,10 @@ class TestDriveTests {
     @Test
     fun execute() {
         val forwardValue = 0.5;
-
-//        val leapfrogDriveBase = mockk<LeapfrogDriveBase>()
-//        val testDrive = TestDrive(leapfrogDriveBase, DoubleSupplier { forwardValue })
-//        testDrive.execute()
-//        verify { leapfrogDriveBase.drive(forwardValue) }
-
+        val leapfrogDriveBase = Mockito.mock<LeapfrogDriveBase>()
+        val testDrive = TestDrive(leapfrogDriveBase, DoubleSupplier { forwardValue })
+        testDrive.execute()
+        verify(leapfrogDriveBase).drive(forwardValue)
     }
 
-    @Test
-    fun end() {
-    }
 }
