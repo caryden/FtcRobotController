0d66235316c66f2b59a4fd944a779274fc60fa2b
Carl Ryden <caryden@alum.mit.edu>
2023-03-07 17:17:11 -0500

    Added simple junit4 tests and test root

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/tests/commands/TestDriveTests.kt
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.tests.commands
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class TestDriveTests {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun execute() {
+        val forwardValue = 0.5;
+
+//        val leapfrogDriveBase = mockk<LeapfrogDriveBase>()
+//        val testDrive = TestDrive(leapfrogDriveBase, DoubleSupplier { forwardValue })
+//        testDrive.execute()
+//        verify { leapfrogDriveBase.drive(forwardValue) }
+
+    }
+
+    @Test
+    fun end() {
+    }
+}
