c1efe237dc7489cab15a09600c254753b6b9f7fe
Carl Ryden <caryden@alum.mit.edu>
2023-03-24 14:34:37 -0400

    Update SwerveDriveBaseTests.kt
    Add additional parameters to the test that I missed in the last commit

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/tests/subsystems/SwerveDriveBaseTests.kt
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/tests/subsystems/SwerveDriveBaseTests.kt
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.tests.subsystems
 
 import com.arcrobotics.ftclib.command.CommandScheduler
+import com.arcrobotics.ftclib.geometry.Pose2d
+import com.arcrobotics.ftclib.geometry.Rotation2d
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.ChassisSpeeds
 import org.firstinspires.ftc.teamcode.subsystems.SwerveDriveBase
 import org.firstinspires.ftc.teamcode.subsystems.SwerveModule
@@ -24,7 +26,7 @@ class SwerveDriveBaseTests {
 
     @Test
     fun drive_should_set_module_states() {
-        val swerveDriveBase = SwerveDriveBase(frontLeftSwerveModule, frontRightSwerveModule, backLeftSwerveModule, backRightSwerveModule)
+        val swerveDriveBase = SwerveDriveBase(frontLeftSwerveModule, frontRightSwerveModule, backLeftSwerveModule, backRightSwerveModule) { Rotation2d(0.0) }
         val chassisSpeeds = ChassisSpeeds(0.1, 0.2, 0.3)
         swerveDriveBase.drive(chassisSpeeds)
         swerveModules.forEach { verify(it).moduleState = any() }
@@ -32,8 +34,8 @@ class SwerveDriveBaseTests {
 
     @Test
     fun initialize_should_initialize_swervemodules() {
-        val swerveDriveBase = SwerveDriveBase(frontLeftSwerveModule, frontRightSwerveModule, backLeftSwerveModule, backRightSwerveModule)
-        swerveDriveBase.initialize()
+        val swerveDriveBase = SwerveDriveBase(frontLeftSwerveModule, frontRightSwerveModule, backLeftSwerveModule, backRightSwerveModule) { Rotation2d(0.0) }
+        swerveDriveBase.initialize(Pose2d(0.0, 0.0, Rotation2d(0.0)))
         swerveModules.forEach { verify(it).initialize() }
     }
 
